df5fc0ded2199c376e03b72120bc87d6ef5ecbd8
daniyalasim3 <daniyalasim3@gmail.com>
2025-07-17 14:50:47 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+    public float speed = 12f;
+    public float gravity = -9.81f * 2;
+    public float jumpHeight = 3f;
+ 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+ 
+    Vector3 velocity;
+ 
+    bool isGrounded;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        //checking if we hit the ground to reset our falling velocity, otherwise we will fall faster the next time
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+ 
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+ 
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+ 
+        Vector3 direction = new Vector3(x, 0f, z).normalized;
+
+        Vector3 camForward = cam.forward;
+        Vector3 camRight = cam.right;
+
+        // Flatten the vectors (ignore vertical tilt)
+camForward.y = 0f;
+camRight.y = 0f;
+camForward.Normalize();
+camRight.Normalize();
+
+Vector3 move = camRight * direction.x + camForward * direction.z;
+ 
+        controller.Move(move * speed * Time.deltaTime);
+ 
+        //check if the player is on the ground so he can jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            //the equation for jumping
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+ 
+        velocity.y += gravity * Time.deltaTime;
+ 
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
